4bc3eb0dd10d433bc9bc4318eb9b95d4c8f5930c
life-tm <life_tm@rambler.ru>
2019-08-16 13:12:58 +0300

    complited_4_5

--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -13,3 +13,13 @@
 
 command1 = 'switchport trunk allowed vlan 1,2,3,5,8'
 command2 = 'switchport trunk allowed vlan 1,3,8,9'
+
+command1 = command1.split()
+command2 = command2.split()
+command1 = command1[-1].split(',')
+command2 = command2[-1].split(',')
+command1 = set(command1)
+command2 = set(command2)
+command3 = command1 | command2
+command3 = list(command3)
+command3[::2]
